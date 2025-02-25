a603626e3592c1d919cd8991c138299bfa392f75
JeffTaylor108 <jeffta1080@gmail.com>
2025-02-25 15:07:25 -0500

    Added view classes

---
+++ src/JobApplicationManagement/View/ApplicationView.java
@@ -0,0 +1,19 @@
+package JobApplicationManagement.View;
+
+import JobApplicationManagement.Controller.ApplicationController;
+
+import javax.swing.*;
+
+public class ApplicationView extends JFrame {
+
+    ApplicationController controller;
+
+    public ApplicationView(ApplicationController controller) {
+        this.controller = controller;
+    }
+
+    public static void updateApplicationView(ApplicationController controller) {
+
+    }
+
+}
---
+++ src/JobApplicationManagement/View/ApplicationsSentView.java
@@ -0,0 +1,19 @@
+package JobApplicationManagement.View;
+
+import JobApplicationManagement.Controller.ApplicationController;
+
+import javax.swing.*;
+
+public class ApplicationsSentView extends JFrame {
+
+    ApplicationController controller;
+
+    public ApplicationsSentView(ApplicationController controller) {
+        this.controller = controller;
+    }
+
+    public static void updateApplicationsSentView(ApplicationController controller) {
+
+    }
+
+}
