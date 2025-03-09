47bc52964ad641913a21b6902785f27f65f58eac
Kmax607 <max.kraus607@gmail.com>
2025-03-08 21:29:34 -0500

    Update LoginInterface.java

--- src/AuthenticationManagement/LoginInterface.java
+++ src/AuthenticationManagement/LoginInterface.java
@@ -1,15 +1,25 @@
 package AuthenticationManagement;
 
+import java.util.Scanner;
+
 public class LoginInterface {
-    public void displayLoginScreen(){
+    private Scanner scanner = new Scanner(System.in);
 
-    }
-    public String[] getUserCredentials(){
-            return null;
-
-    }
-    public void showLoginError(String errorMessage){
-
+    public void displayLoginScreen() {
+        System.out.println("Displaying login screen...");
     }
 
+    public String[] getUserCredentials() {
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+        
+        return new String[]{username, password};
+    }
+
+    public void showLoginError(String errorMessage) {
+        System.out.println("Login Error: " + errorMessage);
+    }
 }
