d6ae9d457724cbf28c00cb38648eedb7e8a3f19e
Kmax607 <max.kraus607@gmail.com>
2025-05-01 16:53:52 -0400

    Update LoginInterface.java

--- src/AuthenticationManagement/LoginInterface.java
+++ src/AuthenticationManagement/LoginInterface.java
@@ -197,10 +197,23 @@ public class LoginInterface extends JFrame {
         JOptionPane.showMessageDialog(this, error);
     }
 
+    @FunctionalInterface
     interface SimpleDocumentListener extends javax.swing.event.DocumentListener {
         void update();
-        @Override default void insertUpdate(javax.swing.event.DocumentEvent e) { update(); }
-        @Override default void removeUpdate(javax.swing.event.DocumentEvent e) { update(); }
-        @Override default void changedUpdate(javax.swing.event.DocumentEvent e) { update(); }
-    }
+    
+        @Override
+        default void insertUpdate(javax.swing.event.DocumentEvent e) {
+            update();
+        }
+    
+        @Override
+        default void removeUpdate(javax.swing.event.DocumentEvent e) {
+            update();
+        }
+    
+        @Override
+        default void changedUpdate(javax.swing.event.DocumentEvent e) {
+            update();
+        }
+}
 }
